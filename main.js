0dd82a1fdfdd57a3e5d434036f248a000bcfb613
Kristijan Vlasic <kristijan.vlasic1@gmail.com>
2021-11-13 12:51:09 +0100

    basic html with a new list input

---
+++ main.js
@@ -0,0 +1,15 @@
+const lists = document.querySelector('#lists')
+const listInput = document.querySelector('#listInput')
+
+let lists = [];
+let todos = [];
+
+//add a new list object to lists array
+function addList() {
+    let newList = { name: listsInput.value }
+}
+
+// render the list
+function render() {
+    lists.innerHTML = ""
+}
